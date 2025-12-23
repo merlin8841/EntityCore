d081f12f921d8f4c748213c6a895cbca00aad51e
merlin8841 <redkris88@msn.com>
2025-12-23 14:31:04 -0500

    Update HopperFiltersModule.java

--- src/main/java/com/entitycore/modules/hoppers/HopperFiltersModule.java
+++ src/main/java/com/entitycore/modules/hoppers/HopperFiltersModule.java
@@ -3,13 +3,13 @@ package com.entitycore.modules.hoppers;
 import com.entitycore.module.Module;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HopperFiltersModule implements Module {
 
     private JavaPlugin plugin;
-    private Listener listener;
+    private HopperFiltersListener listener;
+    private HopperFiltersCommand command;
 
     @Override
     public String getName() {
@@ -21,9 +21,19 @@ public final class HopperFiltersModule implements Module {
         this.plugin = plugin;
 
         HopperFilterStorage.init(plugin);
-        this.listener = new HopperFilterListener(plugin);
 
+        this.listener = new HopperFiltersListener(plugin);
         Bukkit.getPluginManager().registerEvents(this.listener, plugin);
+
+        this.command = new HopperFiltersCommand(plugin, this.listener);
+        if (plugin.getCommand("hf") != null) {
+            plugin.getCommand("hf").setExecutor(this.command);
+            plugin.getCommand("hf").setTabCompleter(this.command);
+        } else {
+            plugin.getLogger().warning("[HopperFilters] Missing command 'hf' in plugin.yml. /hf will not work.");
+        }
+
+        plugin.getLogger().info("[HopperFilters] Enabled.");
     }
 
     @Override
@@ -31,6 +41,10 @@ public final class HopperFiltersModule implements Module {
         if (this.listener != null) {
             HandlerList.unregisterAll(this.listener);
             this.listener = null;
+        }
+        if (this.command != null) {
+            HandlerList.unregisterAll(this.command);
+            this.command = null;
         }
         this.plugin = null;
     }
