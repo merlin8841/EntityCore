2cd4ce67f8cac3c542a3ad5fb5634d038d875258
merlin8841 <redkris88@msn.com>
2025-12-23 18:26:47 -0500

    Update HopperFiltersModule.java

--- src/main/java/com/entitycore/modules/hoppers/HopperFiltersModule.java
+++ src/main/java/com/entitycore/modules/hoppers/HopperFiltersModule.java
@@ -1,24 +1,29 @@
 package com.entitycore.modules.hoppers;
 
+import com.entitycore.module.Module;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class HopperFiltersModule {
+public final class HopperFiltersModule implements Module {
 
-    private final JavaPlugin plugin;
+    private JavaPlugin plugin;
 
     private HopperFilterData data;
     private HopperFiltersMenu menu;
     private HopperFiltersListener listener;
     private HopperFiltersCommand command;
 
-    public HopperFiltersModule(JavaPlugin plugin) {
-        this.plugin = plugin;
+    @Override
+    public String getName() {
+        return "HopperFilters";
     }
 
-    public void onEnable() {
+    @Override
+    public void enable(JavaPlugin plugin) {
+        this.plugin = plugin;
+
         this.data = new HopperFilterData(plugin);
         this.menu = new HopperFiltersMenu(data);
         this.listener = new HopperFiltersListener(data, menu);
@@ -31,17 +36,26 @@ public final class HopperFiltersModule {
             hf.setExecutor(command);
             hf.setTabCompleter(command);
         } else {
-            plugin.getLogger().warning("Command 'hf' missing from plugin.yml");
+            plugin.getLogger().warning("Command 'hf' missing from plugin.yml (HopperFiltersModule).");
         }
+
+        plugin.getLogger().info("[HopperFilters] Enabled.");
     }
 
-    public void onDisable() {
+    @Override
+    public void disable() {
+        if (plugin != null) {
+            plugin.getLogger().info("[HopperFilters] Disabling...");
+        }
+
         if (listener != null) {
             HandlerList.unregisterAll(listener);
         }
+
         data = null;
         menu = null;
         listener = null;
         command = null;
+        plugin = null;
     }
 }
