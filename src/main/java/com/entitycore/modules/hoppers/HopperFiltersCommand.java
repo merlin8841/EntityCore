9fdb1f4897716a6e11c5a829e7daa1c64989d4db
merlin8841 <redkris88@msn.com>
2025-12-23 22:00:19 -0500

    Update HopperFiltersCommand.java

--- src/main/java/com/entitycore/modules/hoppers/HopperFiltersCommand.java
+++ src/main/java/com/entitycore/modules/hoppers/HopperFiltersCommand.java
@@ -2,10 +2,7 @@ package com.entitycore.modules.hoppers;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
