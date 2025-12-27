0f84e321c32b157902556112490e222d158e88fc
merlin8841 <redkris88@msn.com>
2025-12-27 10:25:02 -0500

    Update XpRefundService.java

--- src/main/java/com/entitycore/modules/extendedanvil/XpRefundService.java
+++ src/main/java/com/entitycore/modules/extendedanvil/XpRefundService.java
@@ -2,5 +2,7 @@ package com.entitycore.modules.extendedanvil;
 
 public final class XpRefundService {
 
-    public XpRefundService(ExtendedAnvilConfig config) {}
+    public XpRefundService(ExtendedAnvilConfig config) {
+        // later: implement 75/25/0 refund tracking
+    }
 }
