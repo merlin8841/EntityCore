b54f428a4e6aa32f907b959ce8d8724274c515f4
merlin8841 <redkris88@msn.com>
2025-12-28 12:52:56 -0500

    Update ExtendedAnvilListener.java

--- src/main/java/com/entitycore/modules/extendedanvil/ExtendedAnvilListener.java
+++ src/main/java/com/entitycore/modules/extendedanvil/ExtendedAnvilListener.java
@@ -3,7 +3,9 @@ package com.entitycore.modules.extendedanvil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.*;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -31,7 +33,7 @@ public final class ExtendedAnvilListener implements Listener {
         }
 
         if (holder instanceof ExtendedAnvilAdminGui.Holder) {
-            String title = event.getView().getTopInventory().getTitle();
+            String title = event.getView().getTitle();
 
             if (title.equals("EA Admin")) {
                 ExtendedAnvilAdminGui.handleClick(player, event, plugin, config, service);
