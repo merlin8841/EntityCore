f614eacede44e1fd1f67557a7f894ee5b6a51938
merlin8841 <redkris88@msn.com>
2025-12-28 12:10:06 -0500

    Update ExtendedAnvilGuiModule.java

--- src/main/java/com/entitycore/modules/extendedanvil/ExtendedAnvilGuiModule.java
+++ src/main/java/com/entitycore/modules/extendedanvil/ExtendedAnvilGuiModule.java
@@ -1,39 +1,15 @@
 package com.entitycore.modules.extendedanvil;
 
 import com.entitycore.module.Module;
-import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Extended Anvil (chest GUI) module.
- *
- * Player GUI:
- *  - /ea (permission: entitycore.extendedanvil.use) [requires looking at an anvil]
- * Operator GUI:
- *  - /eaadmin (permission: entitycore.extendedanvil.admin)
- */
 public final class ExtendedAnvilGuiModule implements Module {
 
     private JavaPlugin plugin;
-
     private ExtendedAnvilConfig config;
     private ExtendedAnvilService service;
-
-    private ExtendedAnvilGui playerGui;
-    private ExtendedAnvilAdminMainGui adminMainGui;
-    private ExtendedAnvilRefundGui refundGui;
-    private ExtendedAnvilApplyCostGui applyCostGui;
-    private ExtendedAnvilGeneralGui generalGui;
-    private ExtendedAnvilPriorityGui priorityGui;
-    private ExtendedAnvilEnchantCostGui costGui;
-    private ExtendedAnvilEnchantCapGui capGui;
-
     private ExtendedAnvilListener listener;
-
-    private ExtendedAnvilCommand playerCommand;
-    private ExtendedAnvilAdminCommand adminCommand;
 
     @Override
     public String getName() {
@@ -48,70 +24,34 @@ public final class ExtendedAnvilGuiModule implements Module {
         this.config.load();
 
         this.service = new ExtendedAnvilService(plugin, config);
+        this.listener = new ExtendedAnvilListener(plugin, config, service);
 
-        this.adminMainGui = new ExtendedAnvilAdminMainGui(config);
-        this.refundGui = new ExtendedAnvilRefundGui(config);
-        this.applyCostGui = new ExtendedAnvilApplyCostGui(config);
-        this.generalGui = new ExtendedAnvilGeneralGui(config);
-        this.priorityGui = new ExtendedAnvilPriorityGui(plugin, config);
-        this.costGui = new ExtendedAnvilEnchantCostGui(config);
-        this.capGui = new ExtendedAnvilEnchantCapGui(config);
-        this.playerGui = new ExtendedAnvilGui(plugin, config, service);
-
-        this.listener = new ExtendedAnvilListener(plugin, config, service,
-                playerGui, adminMainGui, refundGui, applyCostGui, generalGui,
-                priorityGui, costGui, capGui);
-        Bukkit.getPluginManager().registerEvents(listener, plugin);
-
-        this.playerCommand = new ExtendedAnvilCommand(playerGui);
-        this.adminCommand = new ExtendedAnvilAdminCommand(adminMainGui);
+        plugin.getServer().getPluginManager().registerEvents(listener, plugin);
 
         PluginCommand ea = plugin.getCommand("ea");
         if (ea != null) {
-            ea.setExecutor(playerCommand);
-            ea.setTabCompleter(playerCommand);
+            ea.setExecutor(new ExtendedAnvilCommand(plugin, config, service));
         } else {
-            plugin.getLogger().warning("Command 'ea' missing from plugin.yml (ExtendedAnvilGuiModule).");
+            plugin.getLogger().severe("[ExtendedAnvil] Command /ea missing from plugin.yml");
         }
 
-        PluginCommand eaAdmin = plugin.getCommand("eaadmin");
-        if (eaAdmin != null) {
-            eaAdmin.setExecutor(adminCommand);
-            eaAdmin.setTabCompleter(adminCommand);
+        PluginCommand eaadmin = plugin.getCommand("eaadmin");
+        if (eaadmin != null) {
+            eaadmin.setExecutor(new ExtendedAnvilAdminCommand(plugin, config, service));
         } else {
-            plugin.getLogger().warning("Command 'eaadmin' missing from plugin.yml (ExtendedAnvilGuiModule).");
+            plugin.getLogger().severe("[ExtendedAnvil] Command /eaadmin missing from plugin.yml");
         }
 
-        plugin.getLogger().info("[ExtendedAnvil] Enabled"
-                + " refund=" + config.getRefundPercentFirst() + "%/"
-                + config.getRefundPercentSecond() + "%/"
-                + config.getRefundPercentLast() + "%"
-                + ", fallbackRefundLvlsPerEnchantLvl=" + config.getRefundLevelsPerEnchantLevel()
-                + ", allowCurseRemoval=" + config.isAllowCurseRemoval()
-                + ", debug=" + config.isDebug()
-                + ", priorWork(cost/inc)=" + config.getPriorWorkCostPerStep() + "/" + config.getPriorWorkIncrementPerApply()
-        );
+        plugin.getLogger().info("[ExtendedAnvil] Enabled.");
     }
 
     @Override
     public void disable() {
-        if (listener != null) {
-            HandlerList.unregisterAll(listener);
+        if (config != null) {
+            config.save();
         }
-
-        plugin = null;
-        config = null;
-        service = null;
-        playerGui = null;
-        adminMainGui = null;
-        refundGui = null;
-        applyCostGui = null;
-        generalGui = null;
-        priorityGui = null;
-        costGui = null;
-        capGui = null;
-        listener = null;
-        playerCommand = null;
-        adminCommand = null;
+        if (plugin != null) {
+            plugin.getLogger().info("[ExtendedAnvil] Disabled.");
+        }
     }
 }
