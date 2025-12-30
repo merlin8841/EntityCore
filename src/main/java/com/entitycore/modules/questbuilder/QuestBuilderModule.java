07dd7793eae01e706ceb2612904f20a1fd95626a
merlin8841 <redkris88@msn.com>
2025-12-30 03:13:12 -0500

    Update QuestBuilderModule.java

--- src/main/java/com/entitycore/modules/questbuilder/QuestBuilderModule.java
+++ src/main/java/com/entitycore/modules/questbuilder/QuestBuilderModule.java
@@ -1,31 +1,53 @@
 package com.entitycore.modules.questbuilder;
 
+import com.entitycore.module.Module;
+import com.entitycore.modules.questbuilder.command.QuestBuilderCommand;
+import com.entitycore.modules.questbuilder.command.QuestTriggerCommand;
 import com.entitycore.modules.questbuilder.listener.QuestBuilderListener;
+import com.entitycore.modules.questbuilder.listener.QuestTriggerListener;
 import com.entitycore.modules.questbuilder.storage.QuestStorage;
+import com.entitycore.modules.questbuilder.trigger.QuestTriggerEngine;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class QuestBuilderModule {
+public final class QuestBuilderModule implements Module {
 
-    private final JavaPlugin plugin;
     private QuestStorage storage;
 
-    public QuestBuilderModule(JavaPlugin plugin) {
-        this.plugin = plugin;
+    @Override
+    public String getName() {
+        return "QuestBuilder";
     }
 
-    public void enable() {
+    @Override
+    public void enable(JavaPlugin plugin) {
         QuestBuilderKeys.init(plugin);
 
-        storage = new QuestStorage(plugin);
+        this.storage = new QuestStorage(plugin);
 
+        QuestTriggerEngine triggerEngine = new QuestTriggerEngine(storage);
+
+        // Commands
+        plugin.getCommand("qb").setExecutor(new QuestBuilderCommand(plugin, storage));
+        plugin.getCommand("entitycore-qb-trigger").setExecutor(new QuestTriggerCommand(storage));
+
+        // Listeners
         Bukkit.getPluginManager().registerEvents(
                 new QuestBuilderListener(plugin, storage),
                 plugin
         );
-
-        plugin.getCommand("qb").setExecutor(
-                new QuestBuilderCommand(plugin, storage)
+        Bukkit.getPluginManager().registerEvents(
+                new QuestTriggerListener(triggerEngine),
+                plugin
         );
+
+        plugin.getLogger().info("[QuestBuilder] Enabled.");
+    }
+
+    @Override
+    public void disable() {
+        if (storage != null) {
+            storage.save();
+        }
     }
 }
