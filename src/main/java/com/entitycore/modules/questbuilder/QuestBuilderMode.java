ba17f6d76dba58d28d7d918e44eb7f5d9a87b9c1
merlin8841 <redkris88@msn.com>
2025-12-30 03:53:50 -0500

    Update QuestBuilderMode.java

--- src/main/java/com/entitycore/modules/questbuilder/QuestBuilderMode.java
+++ src/main/java/com/entitycore/modules/questbuilder/QuestBuilderMode.java
@@ -5,13 +5,20 @@ public enum QuestBuilderMode {
     AREA_POS1,
     AREA_POS2,
     INFO,
-    PREVIEW;
+    PREVIEW,
+    IMPORT_WORLD_EDIT;
 
     public static QuestBuilderMode from(String s) {
+        if (s == null) return POINT;
         try {
             return QuestBuilderMode.valueOf(s.toUpperCase());
         } catch (Exception e) {
             return POINT;
         }
+    }
+
+    public QuestBuilderMode next() {
+        QuestBuilderMode[] vals = values();
+        return vals[(this.ordinal() + 1) % vals.length];
     }
 }
