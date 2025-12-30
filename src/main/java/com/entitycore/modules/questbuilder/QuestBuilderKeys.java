5845c0c7e294d7188ae1a393a5407f1f4d906d59
merlin8841 <redkris88@msn.com>
2025-12-30 03:14:02 -0500

    Update QuestBuilderKeys.java

--- src/main/java/com/entitycore/modules/questbuilder/QuestBuilderKeys.java
+++ src/main/java/com/entitycore/modules/questbuilder/QuestBuilderKeys.java
@@ -10,12 +10,10 @@ public final class QuestBuilderKeys {
     public static NamespacedKey TOOL;
     public static NamespacedKey MODE;
     public static NamespacedKey DRAFT;
-    public static NamespacedKey POINT_INDEX;
 
     public static void init(JavaPlugin plugin) {
         TOOL = new NamespacedKey(plugin, "qb_tool");
         MODE = new NamespacedKey(plugin, "qb_mode");
         DRAFT = new NamespacedKey(plugin, "qb_draft");
-        POINT_INDEX = new NamespacedKey(plugin, "qb_point_index");
     }
 }
